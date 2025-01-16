19d5949d2b5c6ab6aba46bc87ad731e19864ec5e
Tobe Hinata <yangtaihubu@gmail.com>
2025-01-16 17:53:54 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+document.querySelector('.hover-container').addEventListener('click', function () {
+    this.classList.toggle('active');
+});
+document.querySelector('.hover-container').addEventListener('click', function () {
+    document.querySelector('.menu').classList.toggle('active');
+});
+document.querySelector('.hover-container').addEventListener('click', function () {
+    document.querySelector('.face').classList.toggle('active');
+});
+document.querySelector('.hover-container').addEventListener('click', function () {
+    document.querySelector('.profile').classList.toggle('active');
+});
+document.querySelector('.hover-container').addEventListener('click', function () {
+    document.querySelector('.menutwo').classList.toggle('active');
+});
+document.querySelector('.hover-container').addEventListener('click', function () {
+    document.querySelector('.account').classList.toggle('active');
+});
+
+
+
+
